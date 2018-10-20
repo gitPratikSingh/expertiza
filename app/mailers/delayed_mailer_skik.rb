16673692d081814601b9d59d8e05fbe07ae641a9
Pratik Singh <psingh22@ncsu.edu>
2018-10-20 18:00:57 -0400

    test

---
+++ app/mailers/delayed_mailer_skik.rb
@@ -0,0 +1,18 @@
+require 'sidekik'
+
+class OutWorker
+    include Sidekik::Worker
+
+    def perform(complexity)
+        case complexity
+        when "super_hard"
+            sleep 20
+            puts "super hard task, took quite a bit of effort"
+        when "hard"
+            sleep 20
+            puts "hard task, took a bit of work"
+        when "easy"
+            sleep 20
+            puts "easy task, that wasn't a lot of effort"
+    end    
+end
